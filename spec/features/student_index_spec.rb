5cd4c042aee13b13d6ec27fd9780d57b7d5993dd
Kelley Stephens <kelley.stephens@outlook.com>
2014-09-02 15:47:00 -0500

    added called-on date to view

--- spec/features/student_index_spec.rb
+++ spec/features/student_index_spec.rb
@@ -3,4 +3,18 @@ feature "Student Index Page" do
     visit '/students'
     expect(page).to have_content("Students:")
   end
+
+  scenario "displays student and not called on" do
+    Fabricate(:student, name: "Kelley")
+    visit '/students'
+    expect(page).to have_content('Kelley')
+    expect(page).to have_content('Never called on')
+  end
+
+  scenario "displays student and called-on date" do
+    Fabricate(:student, name: "Sarah", called_on: "2014-09-02")
+    visit '/students'
+    expect(page).to have_content('Sarah')
+    expect(page).to have_content('2014-09-02')
+  end
 end
