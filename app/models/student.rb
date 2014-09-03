435f5831670e6793297719406591eaeac345bcbc
Kelley Stephens <kelley.stephens@outlook.com>
2014-09-02 20:22:54 -0500

    unit test for sample

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,11 @@
 class Student < ActiveRecord::Base
   validates_presence_of :name
+
+  default_scope -> { order("id ASC") }
+  scope :not_called_on_today, -> { where("called_on <= ?", DateTime.now.prev_day) }
+
+  def self.pick_random
+    students = Student.not_called_on_today
+    students.sample
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,7 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Student, :type => :model do
   before do
-    Fabricate(:student, name: "Adam")
+    @student_1 = Fabricate(:student, name: "Adam", called_on: DateTime.new(2014, 8, 29))
+    Fabricate(:student, name: "John", called_on: DateTime.new(2014, 9, 1))
+    @student_3 = Fabricate(:student, name: "Kelley", called_on: DateTime.new(2014, 8, 27))
+    Fabricate(:student, name: "Sarah", called_on: DateTime.now)
   end
   it { should validate_presence_of :name }
+
+  it "can return the first valid choice" do
+    Array.any_instance.stub(:sample) do |array|
+      array[0]
+    end
+    expect(Student.pick_random).to eq @student_1
+  end
+
+  it "can return the last valid choice" do
+    Array.any_instance.stub(:sample) do |array|
+      array.last
+    end
+    expect(Student.pick_random).to eq @student_3
+  end
+
 end
